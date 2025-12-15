b9351dc91b9f49d03ae1dc0058fbc22b3eefc3d2
Alex Tzamalis <alextzamalis16@gmail.com>
2025-12-16 00:05:53 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php 
+/**
+ * Landing page, Αρχική σελίδα ιστοσελιδας
+ * 
+ * Η κυρια σελίδα που βλέπουν οι επισκέπτες
+ * 
+ * Με μια διαφορα!, Έαν ο χρήστης ειναι ήδη συνδεδεμένος τοτε τον στέλνουμε στο ανοίστιχο dashboard\
+ * 
+ * @Author Alextzamalis
+ * UEL: 2872177
+ */
+
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
